7240c1402e38bf433ce32cb22c1a0a2b9ab6c1ff
sandramtzd <s.martinez-dominguez@rgu.ac.uk>
2025-07-25 13:56:27 +0100

    Fixed loading issue by adding footer.php content

--- functions.php
+++ functions.php
@@ -1,13 +1,18 @@
 <?php
+// Enqueue theme styles
 function clicc_enqueue_styles() {
     wp_enqueue_style('clicc-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'clicc_enqueue_styles');
 
-function clicc_register_menus() {
-  register_nav_menus( array(
-    'header-menu' => __( 'Header Menu', 'clicc-theme' ),
-  ) );
-}
-add_action( 'init', 'clicc_register_menus' );
+// Theme setup: menus and title tag
+function clicc_theme_setup() {
+    // Let WordPress manage the document title
+    add_theme_support('title-tag');
 
+    // Register navigation menu
+    register_nav_menus(array(
+        'header-menu' => __('Header Menu', 'clicc-theme'),
+    ));
+}
+add_action('after_setup_theme', 'clicc_theme_setup');
