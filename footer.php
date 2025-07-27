6565e38e8d18b8595e0b6a0f1794224cf6f760c8
sandramtzd <s.martinez-dominguez@rgu.ac.uk>
2025-07-27 13:46:15 +0100

    Add footer and style for footer

--- footer.php
+++ footer.php
@@ -1,3 +1,61 @@
+<footer id="site-footer">
+  <section class="accreditation">
+    <h3>Accredited by</h3>
+    <div class="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/care-inspectorate.png" alt="Care Inspectorate">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/soscn.png" alt="SOSCN">
+    </div>
+  </section>
+
+  <section class="footer-links">
+    <div class="column">
+      <h4>SITEMAP</h4>
+      <ul>
+        <li><a href="#">Clubs</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Contact us</a></li>
+      </ul>
+    </div>
+    <div class="column">
+      <h4>COMPANY</h4>
+      <ul>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Media</a></li>
+      </ul>
+    </div>
+    <div class="column">
+      <h4>USEFUL LINKS</h4>
+      <ul>
+        <li><a href="#">Care Inspectorate</a></li>
+        <li><a href="#">SOSCN</a></li>
+      </ul>
+    </div>
+    <div class="column">
+      <h4>LEGAL</h4>
+      <ul>
+        <li><a href="#">Privacy Notice</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+      </ul>
+    </div>
+    <div class="column">
+      <h4>CONTACT US</h4>
+      <address>
+        Community Link Childcare<br>
+        178 Holburn Street, Third Floor,<br>
+        Aberdeen, AB10 6DA<br>
+        <a href="mailto:info@communitylinkchildcare.org.uk">info@communitylinkchildcare.org.uk</a><br>
+        01224 443 880
+      </address>
+    </div>
+  </section>
+
+  <div class="footer-bottom">
+    <p>©2025 Community Link Childcare. All rights reserved. Site by Sandra Martínez Domínguez</p>
+    <p>Registered in Scotland as CLICC Ltd Company No. SC240746, Scottish Charity No. SC034840</p>
+  </div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
 </html>
