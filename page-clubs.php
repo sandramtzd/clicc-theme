60ab66de7175c550af34b51affe8a922b9a3e195
sandramtzd <s.martinez-dominguez@rgu.ac.uk>
2025-08-04 18:55:03 +0100

    Added ACF components to the page-clubs.php and style

--- page-clubs.php
+++ page-clubs.php
@@ -6,9 +6,15 @@ get_header();
 get_template_part('template-parts/hero');
 ?>
 
-<section class="clubs">
-  <div class="content">
-    <!-- Your content here -->
+<section id="clubs-page" class="section">
+  <div class="container">
+    <?php
+    if (have_posts()) :
+      while (have_posts()) : the_post();
+        the_content(); 
+      endwhile;
+    endif;
+    ?>
   </div>
 </section>
 
